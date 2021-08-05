4575c1014583d648b4631f91c0d1244adaaed9e0
Njong392 <njongemy@gmail.com>
2021-08-05 21:44:09 +0100

    Change of db, so that new tasks are added to list

--- todolist.php
+++ todolist.php
@@ -1,31 +1,26 @@
 <?php
 
 //connect to database
-$conn = mysqli_connect('localhost','emy','test1234','task');
+$conn = mysqli_connect('localhost', 'Njong','emy123', 'todo_list');
 
 //check connection
 if(!$conn){
-	echo 'Connection error:' . mysqli_connect_error();
-
+    echo 'Connection error:' . mysqli_connect_error();
 }
 
 //write query for tasks
-$sql = 'SELECT task, id FROM tasks ORDER BY created_on';
+$sql = 'SELECT task,id,created_at FROM Tasks ORDER BY created_at';
 
-// make query and get result
+//make query and get result
 $result = mysqli_query($conn, $sql);
 
-//fetch resulting rows as array
-$tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//fetch resulting rows as an array
+$tasks= mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-//free result from memory
-mysqli_free_result($result);
-
-//close connection
-mysqli_close($conn);
+//it's good practice to free memory using mysqli_free_result();
+//and then closing the connection using mysqli_close();
 
 
-print_r($tasks);
 
 
 ?>
@@ -52,6 +47,21 @@ print_r($tasks);
 
 				<input class="submit" type="submit" value="Add" name="submit" >
 			    </div>
+
+                <br>
+
+                <?php foreach ($tasks as $task){?>
+                    <div class="input">
+                                <h3><?php echo htmlspecialchars($task['task'])?></h3>
+                                <h6><?php echo htmlspecialchars($task['created_at'])?></h6>
+                    </div>
+
+
+                <?php }?>
+
+
+            </div>
+        </div>
 			</body>
 		</head>
 	</html>
